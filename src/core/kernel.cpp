d1eaba2e4f431a49619ec915611af129c6ff5d89
Isira <isira.udattha@gmail.com>
2026-07-11 18:48:24 +0530

    Add types.h

--- src/core/kernel.cpp
+++ src/core/kernel.cpp
@@ -1,5 +1,7 @@
+#include <common/types.h>
+
 void printf(const char* str) {
-    static unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
     for (int i = 0; str[i] != '\0'; i++) {
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
@@ -13,7 +15,7 @@ extern "C" void callConstructors() {
     for (constructor* i = &start_ctors; i != &end_ctors; i++) (*i)();
 }
 
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber) {
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber) {
     printf("Hello, Kernel World!");
     while (1);
 }
