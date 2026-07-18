e7a8288dfa6b5b673708e1c0020173b5b02b89bb
Isira <isira.udattha@gmail.com>
2026-07-18 14:49:28 +0530

    Add const char* as string

--- src/common/types.h
+++ src/common/types.h
@@ -9,5 +9,6 @@ typedef int                     int32_t;
 typedef unsigned int           uint32_t;
 typedef long long int           int64_t;
 typedef unsigned long long int uint64_t;
+typedef const char*              string;
     
 #endif // __TYPES_H__
