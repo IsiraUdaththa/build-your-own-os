8d8cfd4bfefefefa852cb1536813f1b9f35cbe30
Isira <isira.udattha@gmail.com>
2026-07-18 18:04:10 +0530

    Add gui with vga mode

--- src/core/kernel.cpp
+++ src/core/kernel.cpp
@@ -6,6 +6,10 @@
 #include <drivers/keyboard.h>
 #include <drivers/mouse.h>
 #include <drivers/vga.h>
+#include <gui/desktop.h>
+#include <gui/window.h>
+
+#define GRAPHICSMODE
 
 void printf(const char* str) {
     static uint16_t* VideoMemory = (uint16_t*)0xb8000;
@@ -97,13 +101,25 @@ extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber) {
     InterruptManager interrupts(0x20, &gdt);
 
     printf("Initializing Hardware, Stage 1\n");
+    #ifdef GRAPHICSMODE
+        Desktop desktop(320,200, 0x00,0x00,0xA8);
+    #endif
+
     DriverManager drvManager;
-    PrintfKeyboardEventHandler kbhandler;
-    KeyboardDriver keyboard(&interrupts, &kbhandler);
+    #ifdef GRAPHICSMODE
+        KeyboardDriver keyboard(&interrupts, &desktop);
+    #else
+        PrintfKeyboardEventHandler kbhandler;
+        KeyboardDriver keyboard(&interrupts, &kbhandler);
+    #endif
     drvManager.AddDriver(&keyboard);
 
-    MouseToConsole mousehandler;
-    MouseDriver mouse(&interrupts, &mousehandler);
+    #ifdef GRAPHICSMODE
+            MouseDriver mouse(&interrupts, &desktop);
+    #else
+        MouseToConsole mousehandler;
+        MouseDriver mouse(&interrupts, &mousehandler);
+    #endif
     drvManager.AddDriver(&mouse);
 
     PeripheralComponentInterconnectController PCIController;
@@ -114,9 +130,19 @@ extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber) {
     drvManager.ActivateAll();
 
     printf("Initializing Hardware, Stage 3\n");
+    #ifdef GRAPHICSMODE
+        vga.SetMode(320,200,8);
+        Window win1(&desktop, 10,10,20,20, 0xA8,0x00,0x00);
+        desktop.AddChild(&win1);
+        Window win2(&desktop, 40,15,30,30, 0x00,0xA8,0x00);
+        desktop.AddChild(&win2);
+    #endif
+
     interrupts.Activate();
 
-    vga.SetMode(320,200,8);
-    vga.FillRectangle(0,0,320,200,0x00,0x00,0xA8);
-    while (1);
+    while (1) {
+#       ifdef GRAPHICSMODE
+            desktop.Draw(&vga);
+        #endif
+    }
 }
---
+++ src/gui/window.h
@@ -0,0 +1,21 @@
+
+#ifndef __WINDOW_H__
+#define __WINDOW_H__
+
+#include <drivers/mouse.h>
+#include <gui/widget.h>
+
+class Window : public CompositeWidget {
+   protected:
+    bool Dragging;
+
+   public:
+    Window(Widget* parent, int32_t x, int32_t y, int32_t w, int32_t h, uint8_t r, uint8_t g, uint8_t b);
+    ~Window();
+
+    void OnMouseDown(int32_t x, int32_t y, uint8_t button);
+    void OnMouseUp(int32_t x, int32_t y, uint8_t button);
+    void OnMouseMove(int32_t oldx, int32_t oldy, int32_t newx, int32_t newy);
+};
+
+#endif  // __WINDOW_H__
